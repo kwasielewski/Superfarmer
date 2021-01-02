69cdd62f7b373e88f8e9d62a715392fd6612caf1
Krzysiek <krzychuwas@gmail.com>
2021-01-02 16:19:27 +0100

    Rozwój BotKW 1

--- BotKW/bot.cpp
+++ BotKW/bot.cpp
@@ -7,25 +7,31 @@ using namespace std;
 int bot(int s[5][7], int id){
     int decision=0;
     int binDecision[21];
-    int state = 0;
-    bool presence[7];
-
-    for(int i = 0; i < 7; i++){
+    int state = -1;
+    int presence = 0;
+    for(int i = 4; i > -1; i--){
+        presence *=2;
         if(s[id][i]>0){
-            presence[i] = 1;
-        }else{
-            presence[i] = 0;
+            presence++;             // RSPCHdD
         }
     }
-    //określenie stadium gry early, mid, midlate, late
-
+    //określenie stadium gry
+    if(presence <2){
+        state = 0;
+    }
 
 
     //co robić w danym stadium
+    if(state < -1){
+        cout << "Nieokreślono stanu gry\n";
+    }
     if(state == 0){
         if(s[id][0]>6){
             decision = strToDec("11000000110000000000000");
+        }else{
+            decision = 0;
         }
+        return decision;
     }
 
 
