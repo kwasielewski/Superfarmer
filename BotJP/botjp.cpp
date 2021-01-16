99782a838bcbd4fe50fccb5d308313c43fdadcca
jpops <kubablue01@gmail.com>
2021-01-16 18:00:11 +0100

    stworzenie drzewka decyzji

--- BotJP/botjp.cpp
+++ BotJP/botjp.cpp
@@ -11,34 +11,68 @@ long long int botkrolik(int s[5][7], int id)
     const int bank=0;
     int state=0;
     int zapas=0;
-    while((s[bank][0]+(zapas+1))*2<=s[id][0])  ///sprawdzamy, ile królików możemy wydać na wymianę tak, by miec szanse potem odzyskać wszystkie króliki obecne w banku
-    zapas++;
+    while((s[bank][0]+(zapas+1))*2<=s[id][0])  //sprawdzamy, ile królików możemy wydać na wymianę tak, by miec szanse potem odzyskać wszystkie króliki obecne w banku
+        zapas++;
 
-    if(s[bank][0]*2>0)       ///Trzeba dokupić króliki by mmieć szanse odzyskać wszystkie króliki z banku
+    if(s[bank][0]*2>s[id][0])       //   skupiamy się na królikach
     {
-       int wybor;
-       int oferta[7];
-       for(int i=1;i<=6)
-       {
-          if
-       }
-    }
-    else {
+        if(s[id][0]<=6)
+        {
+
+        }
+
+        else         //zakończ turę i czekaj na rozmnozenie królików
+        }
+    else                     // jest wystarczająco dużo królików, staramy się zbudować kompletny zestaw zwierząt
+    {
+
+        ///   <<< sprawdzam czy mogę wygrać w jednym ruchu
+
+        // nie mogę wygrać w jednym ruchu
+        if(nadwyzka>=6)
+        {
+              if(s[id][2]==0 && zapas>=12)
+              {
+              // kup świnię
+              }
+              else {
+
+               if(s[id][1]==0 && zapas>=6)
+                {
+                    //kup owcę
+                }
+
+                else{
+
+                    if(///mam dużo owiec i swin)
+                    {
+                    ///kup krowę
+                    }
+                    else {
+
+                    ///dokup swinie i owce
+                    }
+
+
+                }
+
+
+              }
 
 
 
-
-    }
-
-
+        }
+        else // zakończ turę
 
 
+        }
 }
 
 
 
-int main(){
-    int tab[5][7]={{100, 100, 100, 100, 100, 100, 100}, {100, 100, 100, 100, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0}};
+int main()
+{
+    int tab[5][7]= {{100, 100, 100, 100, 100, 100, 100}, {100, 100, 100, 100, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0}};
 
     cout << bot(tab, 1);
 
@@ -47,7 +81,8 @@ int main(){
 
 
 
-long long int possible(long long int decision, int s[5][7]){
+long long int possible(long long int decision, int s[5][7])
+{
     //jeśli pomijam to git
 
     int code[10];
@@ -80,17 +115,22 @@ long long int possible(long long int decision, int s[5][7]){
 
     code[0]= decision;
 
-    if(code[0] == 0){
+    if(code[0] == 0)
+    {
         return 0;
     }
     //jeśli się nie da kupić bo nie ma to wyzeruj
-    if(code[1] == 1 && s[0][code[2]+1]==0){
+    if(code[1] == 1 && s[0][code[2]+1]==0)
+    {
         return 0;
     }
     //jeśli się sprzedaje to wyzeruj
-    if(code[1] == 0 ){
-        for(int  i = 0; i < 7 ;i++){
-            if(code[3+i]>s[0][i]){
+    if(code[1] == 0 )
+    {
+        for(int  i = 0; i < 7 ; i++)
+        {
+            if(code[3+i]>s[0][i])
+            {
                 return 0;
             }
         }
@@ -98,34 +138,35 @@ long long int possible(long long int decision, int s[5][7]){
     return decision;
 }
 
-long long int decisionMaker(int exchange, int buy, int big, int rabbit, int sheep, int pig, int cow, int horse, int sDog, int bDog, int s[5][7]){
-	long long int output = 0;
+long long int decisionMaker(int exchange, int buy, int big, int rabbit, int sheep, int pig, int cow, int horse, int sDog, int bDog, int s[5][7])
+{
+    long long int output = 0;
 
-	output += exchange*2+buy;
+    output += exchange*2+buy;
 
-	output *= 8;
-	output = output+big;
+    output *= 8;
+    output = output+big;
 
-	output *= 64;
-	output += rabbit;
+    output *= 64;
+    output += rabbit;
 
-	output *= 32;
-	output += sheep;
+    output *= 32;
+    output += sheep;
 
-	output *= 32;
-	output += pig;
+    output *= 32;
+    output += pig;
 
-	output *= 16;
-	output += cow;
+    output *= 16;
+    output += cow;
 
-	output *= 8;
-	output += horse;
+    output *= 8;
+    output += horse;
 
-	output *= 8;
-	output += sDog;
+    output *= 8;
+    output += sDog;
 
-	output *= 4;
-	output += bDog;
+    output *= 4;
+    output += bDog;
 
-	return possible(output,  s);
+    return possible(output,  s);
 }
