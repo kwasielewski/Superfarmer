1a936ab2f42fb5b8d37535f2e28991f74c1c89d4
elemgie <git>
2021-01-20 01:00:39 +0100

    Dodano linkowanie botów do miejsc przy stole w funkcji usadzGraczy

--- Host/host.cpp
+++ Host/host.cpp
@@ -9,16 +9,23 @@ using namespace std;
 #include "bot3.h"
 #include "bot4.h"
 #include "randomize.h"*/
+//Boty z załączanych plików nagłówkowych
+extern int krolikowy(const int tab[5][7], int id);
+extern int randomizowany(const int tab[5][7], int id);
+extern int usainbolt(const int tab[5][7], int id);
+extern int swinski(const int tab[5][7], int id);
+extern int dokonajWymiany(const int tab[5][7], int id);
 typedef pair<int, int> PII;
+int (*bots[4])(const int[5][7], int); //wskaźniki na boty
 int losuj() //funkcja do zastąpienia funkcją z modułu losującego - do testowania hosta
 {
   srand(getpid() + time(NULL));
   return rand();
 }
-int* usadzGraczy(bool CzyCzlowiekGra)
+static int* usadzGraczy(bool CzyCzlowiekGra)
 {
   int *Usadzenie = new int[4]; //tabela zawierająca kolejność rozgrywki poczynając od gracza pod indeksem 1
-  //Gracze są oznaczani jako wartości: 1 - człowiek, 2 - bot królikowy, 3 - bot randomizowany, 4 - bot zrównoważony, 5 - bot świński
+  //Gracze są oznaczani jako wartości: 1 - człowiek, 2 - bot królikowy, 3 - bot randomizowany, 4 - bot Usain Bolt, 5 - bot świński
   int wykluczony = 1; //id gracza który nie weźmie udziału w danej rozgrywce, defaultowo człowiek
   if(CzyCzlowiekGra)
     wykluczony = 2 + losuj()%4;
@@ -27,9 +34,30 @@ int* usadzGraczy(bool CzyCzlowiekGra)
     if(i != wykluczony)
       Usadzenie[cnt++] = i;
   random_shuffle(Usadzenie, Usadzenie + 4);
+  for(int i = 0; i < 4; i++)
+    switch (Usadzenie[i])
+    {
+      case 1: 
+        bots[i] = dokonajWymiany;
+        break;
+      case 2: 
+        bots[i] = krolikowy;
+        break;
+      case 3:
+        bots[i] = randomizowany;
+        break;
+      case 4:
+        bots[i] = usainbolt;
+        break;
+      case 5:
+        bots[i] = swinski;
+        break;
+      default:
+        break;
+    }
   return Usadzenie;
 }
-int** inicjujStada()
+static int** inicjujStada()
 {
   int **Stado = new int*[5];
   for(int i = 0; i <= 4; i++)
@@ -39,6 +67,10 @@ int** inicjujStada()
     for(int j = 0; j <= 6; j++)
       Stado[i][j] = 0;
   return Stado;
+}
+bool rozpocznijGre(int Usadzenie[4], int Stado[5][7])
+{
+  //Construction site
 }
 int main() //w wersji release nie będzie tej funkcji - służy testowaniu hosta
 {
