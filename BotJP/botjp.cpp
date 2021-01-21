48236ce4853775da030a85bff66fd60f769c2980
jpops <kubablue01@gmail.com>
2021-01-21 17:48:07 +0100

    poprawiono tworzenie decyzji

--- BotJP/botjp.cpp
+++ BotJP/botjp.cpp
@@ -91,10 +91,7 @@ int main()
     return 0;
 }
 
-
-
-long long int possible(long long int decision, int s[5][7])
-{
+long long int possible(long long int decision, int s[5][7]){
     //jeśli pomijam to git
 
     int code[10];
@@ -126,23 +123,22 @@ long long int possible(long long int decision, int s[5][7])
     decision/=2;
 
     code[0]= decision;
-
-    if(code[0] == 0)
-    {
+    /*cout << "Odzyskany kod ";
+	for(int i = 0; i< 10; i ++){
+		cout << code[i] << " ";
+	}
+	cout << endl;*/
+    if(code[0] == 0){
         return 0;
     }
     //jeśli się nie da kupić bo nie ma to wyzeruj
-    if(code[1] == 1 && s[0][code[2]+1]==0)
-    {
+    if(code[1] == 1 && s[0][code[2]+1]==0){
         return 0;
     }
     //jeśli się sprzedaje to wyzeruj
-    if(code[1] == 0 )
-    {
-        for(int  i = 0; i < 7 ; i++)
-        {
-            if(code[3+i]>s[0][i])
-            {
+    if(code[1] == 0 ){
+        for(int  i = 0; i < 7 ;i++){
+            if(code[3+i]>s[0][i]){
                 return 0;
             }
         }
@@ -150,35 +146,37 @@ long long int possible(long long int decision, int s[5][7])
     return decision;
 }
 
-long long int decisionMaker(int exchange, int buy, int big, int rabbit, int sheep, int pig, int cow, int horse, int sDog, int bDog, int s[5][7])
-{
-    long long int output = 0;
+long long int decisionMaker(int exchange, int buy, int big, int rabbit, int sheep, int pig, int cow, int horse, int sDog, int bDog, int s[5][7]){
+	cout << "Ex: " << exchange << " Buy: " << buy << " Big: " << big << " Num: " << rabbit << " " << sheep << " " << pig << " " << cow << " "  <<  horse << " " << sDog << " " << bDog <<endl;
+	long long int output = 0;
 
-    output += exchange*2+buy;
+	output += exchange*2+buy;
 
-    output *= 8;
-    output = output+big;
+	output *= 8;
+	output = output+big;
 
-    output *= 64;
-    output += rabbit;
+	output *= 64;
+	output += rabbit;
 
-    output *= 32;
-    output += sheep;
+	output *= 32;
+	output += sheep;
 
-    output *= 32;
-    output += pig;
+	output *= 32;
+	output += pig;
 
-    output *= 16;
-    output += cow;
+	output *= 16;
+	output += cow;
 
-    output *= 8;
-    output += horse;
+	output *= 8;
+	output += horse;
 
-    output *= 8;
-    output += sDog;
+	output *= 8;
+	output += sDog;
 
-    output *= 4;
-    output += bDog;
+	output *= 4;
+	output += bDog;
 
-    return possible(output,  s);
+	return possible(output,  s) ? output : 0;
 }
+
+
