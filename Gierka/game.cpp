a97e8664dfe23967acf02790514d2d4be706b042
Krzysiek <krzychuwas@gmail.com>
2021-01-24 19:53:29 +0100

    Próby ogarnięcia

---
+++ Gierka/game.cpp
@@ -0,0 +1,322 @@
+#include <iostream>
+#include <gtkmm.h>
+using namespace std;
+
+/*
+To jest program do zarządzania interfejsem użytkownika do gry Superfarmer.
+Znajdą się tu opisy nastêpujących okien:
+ > główne okno (informacje o stanie plansz wszystkich graczy):
+    - tabela GTK_GRID?
+    - przycisk, który umożliwia wymianę
+    - przycisk do "rzutu kostką"
+    - miejsce, gdzie wyświetli się wynik tego rzutu
+ > okno pozwalające na wymianę z bankiem (stadem głównym)
+    - wybór kupno czy sprzedaż (może zaznaczanie ptaszkiem kwadracika?)
+    - drop-down menu, które pozwala graczowi wybrać, jakie zwierzę chce nabyć GTK_COMBO_BOX?
+    - możliwość wyboru jakie dokładnie zwierzaki gracz chce wymienić
+    - przycisk do zatwierdzenia wyboru
+    - przycisk do anulowania wymiany
+ > ewentualny pop-up z nformacją o błędzie (czy da się ustawić czerwone tło?)
+    - gracz ma niewystaczającą liczbę zwierząt na wymianę
+    - informacja o końcu gry (i kto wygrał rozgrywkę)
+*/
+
+struct element_tabeli
+{
+    const char *opis;
+    int pozycja_kolumna, pozycja_wiersz;
+};
+element_tabeli tabela_wewnatrz[10][10];
+GtkWidget *tabela;
+
+void Dokonaj_Wymiany();
+void Wymiana_Zatwierdzona(int *wybor);
+void Blad_Wymiana_Niemozliwa();
+void Rzut_Kostka();
+void Koniec_Gry();
+
+GtkWidget *okno_wymiana;
+
+extern long long (*bots[4])(int*[5], int);
+
+
+void Dokonaj_Wymiany()
+{
+    okno_wymiana = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW(okno_wymiana), "Wymiana");
+    gtk_window_set_position(GTK_WINDOW(okno_wymiana), GTK_WIN_POS_CENTER);
+    gtk_container_set_border_width(GTK_CONTAINER(okno_wymiana), 30);
+    g_signal_connect(G_OBJECT(okno_wymiana), "destroy", G_CALLBACK(gtk_widget_destroy), okno_wymiana);
+
+    GtkWidget *menu;
+    menu = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
+    gtk_container_set_border_width (GTK_CONTAINER (menu), 30);
+
+    int wybor_gracza[10];
+
+    GtkWidget *kupno_czy_sprzedaz = gtk_combo_box_text_new();
+    const char *k_czy_p[] = {"Kupuję", "Sprzedaję"};
+    for (int i = 0; i < 2; i++) gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(kupno_czy_sprzedaz), k_czy_p[i]);
+    gtk_combo_box_set_active(GTK_COMBO_BOX(kupno_czy_sprzedaz), 0);
+    gtk_container_add(GTK_CONTAINER(menu), kupno_czy_sprzedaz);
+    wybor_gracza[0] = gtk_combo_box_get_active(GTK_COMBO_BOX(kupno_czy_sprzedaz));
+
+    GtkWidget *wymiana_tekst = gtk_label_new("Jakie zwierzę chcesz kupić/sprzedać?");
+    gtk_container_add(GTK_CONTAINER(menu), wymiana_tekst);
+
+    GtkWidget *wybierz_zwierze = gtk_combo_box_text_new();
+    const char *zwierzeta[] = {"Wybierz zwierzę", "królik", "owca", "świnia", "krowa", "koń", "mały pies", "duży pies"};
+    for (int i = 0; i < 8; i++) gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(wybierz_zwierze), zwierzeta[i]);
+    gtk_combo_box_set_active(GTK_COMBO_BOX(wybierz_zwierze), 0);
+    gtk_container_add(GTK_CONTAINER(menu), wybierz_zwierze);
+    wybor_gracza[1] = gtk_combo_box_get_active(GTK_COMBO_BOX(wybierz_zwierze));
+
+    GtkWidget *ile_zwierzat = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 10);
+    gtk_container_set_border_width (GTK_CONTAINER (ile_zwierzat), 30);
+    gtk_container_add(GTK_CONTAINER(menu), ile_zwierzat);
+
+    const char *liczby[] = {"0", "1", "2", "3", "4", "5", "6"};
+
+    GtkWidget *ile_krolikow = gtk_combo_box_text_new();
+    for (int i = 0; i < 7; i++) gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(ile_krolikow), liczby[i]);
+    gtk_combo_box_set_active(GTK_COMBO_BOX(ile_krolikow), 0);
+    gtk_container_add(GTK_CONTAINER(ile_zwierzat), ile_krolikow);
+    wybor_gracza[2] = gtk_combo_box_get_active(GTK_COMBO_BOX(ile_krolikow));
+
+    GtkWidget *ile_owiec = gtk_combo_box_text_new();
+    for (int i = 0; i < 7; i++) gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(ile_owiec), liczby[i]);
+    gtk_combo_box_set_active(GTK_COMBO_BOX(ile_owiec), 0);
+    gtk_container_add(GTK_CONTAINER(ile_zwierzat), ile_owiec);
+    wybor_gracza[3] = gtk_combo_box_get_active(GTK_COMBO_BOX(ile_owiec));
+
+    GtkWidget *ile_swin = gtk_combo_box_text_new();
+    for (int i = 0; i < 7; i++) gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(ile_swin), liczby[i]);
+    gtk_combo_box_set_active(GTK_COMBO_BOX(ile_swin), 0);
+    gtk_container_add(GTK_CONTAINER(ile_zwierzat), ile_swin);
+    wybor_gracza[4] = gtk_combo_box_get_active(GTK_COMBO_BOX(ile_swin));
+
+    GtkWidget *ile_krow = gtk_combo_box_text_new();
+    for (int i = 0; i < 7; i++) gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(ile_krow), liczby[i]);
+    gtk_combo_box_set_active(GTK_COMBO_BOX(ile_krow), 0);
+    gtk_container_add(GTK_CONTAINER(ile_zwierzat), ile_krow);
+    wybor_gracza[5] = gtk_combo_box_get_active(GTK_COMBO_BOX(ile_krow));
+
+    GtkWidget *ile_koni = gtk_combo_box_text_new();
+    for (int i = 0; i < 7; i++) gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(ile_koni), liczby[i]);
+    gtk_combo_box_set_active(GTK_COMBO_BOX(ile_koni), 0);
+    gtk_container_add(GTK_CONTAINER(ile_zwierzat), ile_koni);
+    wybor_gracza[6] = gtk_combo_box_get_active(GTK_COMBO_BOX(ile_koni));
+
+    GtkWidget *ile_malych_psow = gtk_combo_box_text_new();
+    for (int i = 0; i < 7; i++) gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(ile_malych_psow), liczby[i]);
+    gtk_combo_box_set_active(GTK_COMBO_BOX(ile_malych_psow), 0);
+    gtk_container_add(GTK_CONTAINER(ile_zwierzat), ile_malych_psow);
+    wybor_gracza[7] = gtk_combo_box_get_active(GTK_COMBO_BOX(ile_malych_psow));
+
+    GtkWidget *ile_duzych_psow = gtk_combo_box_text_new();
+    for (int i = 0; i < 7; i++) gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(ile_duzych_psow), liczby[i]);
+    gtk_combo_box_set_active(GTK_COMBO_BOX(ile_duzych_psow), 0);
+    gtk_container_add(GTK_CONTAINER(ile_zwierzat), ile_duzych_psow);
+    wybor_gracza[8] = gtk_combo_box_get_active(GTK_COMBO_BOX(ile_duzych_psow));
+
+    GtkWidget *przycisk_zatwierdz = gtk_button_new_with_label("Zatwierdź");
+    gtk_container_add(GTK_CONTAINER(menu), przycisk_zatwierdz);
+    g_signal_connect (G_OBJECT(przycisk_zatwierdz), "clicked", G_CALLBACK(Wymiana_Zatwierdzona), wybor_gracza); //wywołać z argumentem - wybrana opcja
+
+    GtkWidget *przycisk_anuluj = gtk_button_new_with_label("Anuluj");
+    gtk_container_add(GTK_CONTAINER(menu), przycisk_anuluj);
+    g_signal_connect_swapped(G_OBJECT(przycisk_anuluj), "clicked", G_CALLBACK(gtk_widget_destroy), okno_wymiana);
+
+    gtk_container_add(GTK_CONTAINER(okno_wymiana), menu);
+    gtk_widget_show_all(okno_wymiana);
+}
+
+void Wymiana_Zatwierdzona(int *wybor) //wybrane zwierzę jako argument
+{
+    ///połączyć z hostem gry
+    ///musi potem nastąpić update tabeli w glowny_wewnatrz
+
+    ///wybor[0] = 0 jeśli sprzedaż
+    ///           1 jeśli kupno
+
+    ///wybor[1] = jaki zwierzak
+
+    ///wybor[2...8] = ile każdego z nich
+
+    bool czy_wymiana_mozliwa = true; //testowe wywołanie (do zmiany później) = czyWymianaPoprawna()
+
+    if (czy_wymiana_mozliwa)
+    {
+        ///tutaj update
+        ///ta funkcja powinna automatycznie zamknąć okno "Wymiana"
+
+        for (int i = 1; i < 8; i++)
+        {
+            for (int j = 1; j < 5; j++)
+            {
+                GtkLabel *modyfikowany_opis = (GtkLabel*)gtk_grid_get_child_at(GTK_GRID(tabela), i, j);
+                gtk_label_set_text(modyfikowany_opis, tabela_wewnatrz[i][j].opis);
+            }
+        }
+
+        gtk_widget_destroy(okno_wymiana);
+
+    }
+    else Blad_Wymiana_Niemozliwa();
+}
+
+void Blad_Wymiana_Niemozliwa()
+{
+    GtkWidget *okno_wymiana_niemozliwa = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW(okno_wymiana_niemozliwa), "Ups!");
+    gtk_window_set_position(GTK_WINDOW(okno_wymiana_niemozliwa), GTK_WIN_POS_CENTER);
+    gtk_container_set_border_width(GTK_CONTAINER(okno_wymiana_niemozliwa), 30);
+    g_signal_connect(G_OBJECT(okno_wymiana_niemozliwa), "destroy", G_CALLBACK(gtk_widget_destroy), okno_wymiana_niemozliwa);
+
+    GtkWidget *own_wewnatrz = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
+    gtk_container_set_border_width (GTK_CONTAINER (own_wewnatrz), 30);
+    gtk_container_add(GTK_CONTAINER(okno_wymiana_niemozliwa), own_wewnatrz);
+
+    GtkWidget *own_tekst = gtk_label_new("Nie masz wystarczających środków na taką wymianę");
+    gtk_container_add(GTK_CONTAINER(own_wewnatrz), own_tekst);
+
+    GtkWidget *przycisk_rozumiem = gtk_button_new_with_label("Rozumiem");
+    gtk_container_add(GTK_CONTAINER(own_wewnatrz), przycisk_rozumiem);
+    g_signal_connect_swapped(G_OBJECT(przycisk_rozumiem), "clicked", G_CALLBACK(gtk_widget_destroy), okno_wymiana_niemozliwa);
+
+    gtk_widget_show_all(okno_wymiana_niemozliwa);
+}
+
+void Rzut_Kostka() //wskaźnik na tabelę/okno główne jako argument?
+{
+    ///połączyć z hostem gry i generatorem liczb losowych
+    ///wyświetlić komunikat o wyniku losowania w oknie głównym
+    ///musi potem nastąpić update tabeli w glowny_wewnatrz
+    ///stąd powinna też przyjść informacja o ewentualnym końcu rozgrywki (wywołanie void Koniec_Gry())
+
+    for (int i = 1; i < 8; i++)
+    {
+        for (int j = 1; j < 5; j++)
+        {
+            GtkLabel *modyfikowany_opis = (GtkLabel*)gtk_grid_get_child_at(GTK_GRID(tabela), i, j);
+            gtk_label_set_text(modyfikowany_opis, tabela_wewnatrz[i][j].opis);
+        }
+    }
+}
+
+void Koniec_Gry()
+{
+    ///połączyć z hostem gry - wygrana czy przegrana?
+
+    bool czy_wygrales = false; //tylko testowa wartość, tutaj wywołać trzeba funkcję z hosta
+
+    GtkWidget *okno_koniec_gry = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW(okno_koniec_gry), "Koniec gry");
+    gtk_window_set_position(GTK_WINDOW(okno_koniec_gry), GTK_WIN_POS_CENTER);
+    gtk_container_set_border_width(GTK_CONTAINER(okno_koniec_gry), 30);
+    g_signal_connect(G_OBJECT(okno_koniec_gry), "destroy", G_CALLBACK(gtk_main_quit), NULL);
+
+    GtkWidget *okg_wewnatrz = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
+    gtk_container_set_border_width (GTK_CONTAINER (okg_wewnatrz), 30);
+    gtk_container_add(GTK_CONTAINER(okno_koniec_gry), okg_wewnatrz);
+
+    GtkWidget *info_o_wyniku = gtk_label_new(NULL);
+    if (czy_wygrales) gtk_label_set_text(GTK_LABEL(info_o_wyniku), "Wygrałeś! Gratulacje!");
+    else gtk_label_set_text(GTK_LABEL(info_o_wyniku), "Przegrałeś");
+    gtk_container_add(GTK_CONTAINER(okg_wewnatrz), info_o_wyniku);
+
+    GtkWidget *przycisk_ok = gtk_button_new_with_label("OK");
+    gtk_container_add(GTK_CONTAINER(okg_wewnatrz), przycisk_ok);
+    g_signal_connect_swapped(G_OBJECT(przycisk_ok), "clicked", G_CALLBACK(gtk_widget_destroy), okno_koniec_gry);
+
+    gtk_widget_show_all(okno_koniec_gry);
+}
+
+int main(int argc, char *argv[])
+{
+    string s;
+    if(argc < 2)
+        s = "dane1.csv";
+    else{
+        s = argv[1];
+        s="dane"+s+".csv";
+    }
+
+
+
+
+    gtk_init(&argc, &argv);
+    GtkWidget *glowny = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW(glowny), "Superfarmer");
+    gtk_window_set_position(GTK_WINDOW(glowny), GTK_WIN_POS_CENTER);
+    gtk_container_set_border_width(GTK_CONTAINER(glowny), 30);
+    g_signal_connect(G_OBJECT(glowny), "destroy", G_CALLBACK(gtk_main_quit), NULL);
+
+    GtkWidget *glowny_wewnatrz;
+    glowny_wewnatrz = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 10);
+
+    tabela = gtk_grid_new();
+    gtk_grid_set_row_spacing(GTK_GRID(tabela), 3);
+    gtk_grid_set_row_homogeneous(GTK_GRID(tabela), TRUE);
+    gtk_grid_set_column_spacing(GTK_GRID(tabela), 3);
+    gtk_grid_set_column_homogeneous(GTK_GRID(tabela), TRUE);
+
+    tabela_wewnatrz[1][0].opis = "króliki";
+    tabela_wewnatrz[2][0].opis = "owce";
+    tabela_wewnatrz[3][0].opis = "świnie";
+    tabela_wewnatrz[4][0].opis = "krowy";
+    tabela_wewnatrz[5][0].opis = "konie";
+    tabela_wewnatrz[6][0].opis = "małe psy";
+    tabela_wewnatrz[7][0].opis = "duże psy";
+
+    for (int i = 1; i < 8; i++)
+    {
+        tabela_wewnatrz[i][0].pozycja_kolumna = i;
+        tabela_wewnatrz[i][0].pozycja_wiersz = 0;
+        GtkWidget *komorka = gtk_label_new(NULL);
+        gtk_label_set_text(GTK_LABEL(komorka), tabela_wewnatrz[i][0].opis);
+        gtk_grid_attach(GTK_GRID(tabela), komorka, tabela_wewnatrz[i][0].pozycja_kolumna, tabela_wewnatrz[i][0].pozycja_wiersz, 1, 1);
+    }
+
+    tabela_wewnatrz[0][1].opis = "Gracz 1";
+    tabela_wewnatrz[0][2].opis = "Gracz 2";
+    tabela_wewnatrz[0][3].opis = "Gracz 3";
+    tabela_wewnatrz[0][4].opis = "Gracz 4";
+
+    for (int i = 1; i < 5; i++)
+    {
+        tabela_wewnatrz[0][i].pozycja_kolumna = 0;
+        tabela_wewnatrz[0][i].pozycja_wiersz = i;
+        GtkWidget *komorka = gtk_label_new(NULL);
+        gtk_label_set_text(GTK_LABEL(komorka), tabela_wewnatrz[0][i].opis);
+        gtk_grid_attach(GTK_GRID(tabela), komorka, tabela_wewnatrz[0][i].pozycja_kolumna, tabela_wewnatrz[0][i].pozycja_wiersz, 1, 1);
+    }
+
+    for (int i = 1; i < 8; i++)
+    {
+        for (int j = 1; j < 5; j++)
+        {
+            tabela_wewnatrz[i][j].opis = "0";
+            tabela_wewnatrz[i][j].pozycja_kolumna = i;
+            tabela_wewnatrz[i][j].pozycja_wiersz = j;
+            GtkWidget *komorka = gtk_label_new(NULL);
+            gtk_label_set_text(GTK_LABEL(komorka), tabela_wewnatrz[i][j].opis);
+            gtk_grid_attach(GTK_GRID(tabela), komorka, tabela_wewnatrz[i][j].pozycja_kolumna, tabela_wewnatrz[i][j].pozycja_wiersz, 1, 1);
+        }
+    }
+
+    gtk_container_add(GTK_CONTAINER(glowny_wewnatrz), tabela);
+
+    GtkWidget *przycisk_wymiana = gtk_button_new_with_label("Chcę dokonać wymiany");
+    gtk_container_add(GTK_CONTAINER(glowny_wewnatrz), przycisk_wymiana);
+    g_signal_connect (G_OBJECT(przycisk_wymiana), "clicked", G_CALLBACK(Dokonaj_Wymiany), tabela_wewnatrz);
+
+    GtkWidget *przycisk_rzut = gtk_button_new_with_label("Rzut kostką");
+    gtk_container_add(GTK_CONTAINER(glowny_wewnatrz), przycisk_rzut);
+    g_signal_connect (G_OBJECT(przycisk_rzut), "clicked", G_CALLBACK(Rzut_Kostka), NULL); //tablica jako argument
+    gtk_container_add(GTK_CONTAINER(glowny), glowny_wewnatrz);
+
+    gtk_widget_show_all(glowny);
+    gtk_main();
+}
+
---
+++ Gierka/gosp.cpp
@@ -0,0 +1,326 @@
+//Mateusz Gieroba
+#include <bits/stdc++.h>
+//#include <unistd.h> //do uzyskania ziarna na etapie testowym hosta
+using namespace std;
+//Program ten będzie kompilowany wraz z głównym programem zawierającym interfejs w GTK. Wszelkie funkcje hosta będą
+//wywoływane przez główny program do obsługi przebiegu rozgrywki
+
+extern long long krolikowy(int *Stado[5], int id);
+extern long long randomizowany(int *Stado[5], int id);
+extern long long botQn(int *Stado[5], int id); //AKA usainbolt, highdollar
+extern long long swinski(int *Stado[5], int id);
+extern void slosuj();
+extern int losowanko();
+long long (*bots[4])(int*[5], int); //wskaźniki na boty
+
+ofstream zapisDoPliku;
+int *Usadzenie;
+int **Stado;
+int *Wymiana;
+int zwyciezca = -1, kostka1, kostka2, cnt = 0, nrrundy = 0, czlowiek = -1;
+long long kodWymiany = 0;
+
+static void inicjujZapisDoPliku(ofstream &plik, int Usadzenie[4])
+{
+  plik << "LP,R1,S1,P1,C1,H1,d1,D1,R2,S2,P2,C2,H2,d2,D2,R3,S3,P3,C3,H3,d3,D3,R4,S4,P4,C4,H4,d4,D4,T1,T2,T3,T4\n0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,";
+  for(int i = 0; i < 3; i++)
+    plik << Usadzenie[i] << ',';
+  plik << Usadzenie[3] << '\n';
+}
+
+static void zapiszRunde(ofstream &plik, int *Stado[5], int nrrundy)
+{
+  plik << nrrundy << ',';
+  for(int i = 1; i < 5; i++)
+    for(int j = 0; j < 7; j++)
+      plik << Stado[i][j] << ',';
+  plik << ",,,\n";
+}
+
+void Koniec_Gry(int zwyciezca, bool czyWygralCzlowiek){
+  if(zwyciezca == -1)
+    //cout << "GRA ZAKOŃCZYŁA SIĘ W WYNIKU BŁĘDU!\n";
+  if(zwyciezca == 0)
+    //cout << "DOSZŁO DO REMISU\n";
+  if(czyWygralCzlowiek)
+    //cout << "WYGRAŁEŚ! GRATULACJE!\n";
+  if(!czyWygralCzlowiek && zwyciezca > 0)
+    //cout << "PRZEGRAŁEŚ! WYGRAŁ GRACZ NUMER " << zwyciezca << '\n';
+  return;
+} //wywołanie komunikatu o zakończeniu gry w GTK, w obecnej postaci funkcja do testowania hosta
+void wymianaZatwierdzona(int *Stado[5]) {return;} //wyświetlenie informacji o zaakceptowaniu wymiany oraz aktualizacja planszy
+
+
+
+
+static bool czyWymianaPoprawna(int *Stado[5], int id, int Wymiana[9])
+//funkcja sprawdza poprawność żądań z tabelą wymian i dostosowuje liczby do możliwości stada głównego
+//tj. może pogorszyć liczby z żądania gracza, lecz ten miał pełną informację na temat zasobności stada głównego żądając wymiany (wiedział ile może dostać)
+{
+  int krolikojednostki[7] = {1, 6, 12, 36, 72, 6, 36}; //wartości zwierząt w przeliczeniu na króliki
+  if(Wymiana[0] == 0)
+  {
+    if(Stado[id][Wymiana[1]] == 0)
+      return false; //gracz nie posiada sprzedawanego zwierzęcia
+    int otrzymywane = 0;
+    for(int i = 2; i <= 8; i++)
+      otrzymywane += Wymiana[i]*krolikojednostki[i - 2];
+    if(otrzymywane > krolikojednostki[Wymiana[1]])
+      return false; //zwierzęta żądane za sprzedawane są droższe niż sprzedawane (wg przeliczników)
+    for(int i = 2; i <= 8; i++)
+      Wymiana[i] = min(Wymiana[i], Stado[0][i - 2]); //zmiana żądania danego zwierzęcia jeżeli stado główne ma ich mniej niż żądanie (gracz wiedział w momencie decyzji ile jest ich w stadzie więc świadomie podjął te decyzje)
+    return true;
+  }
+  else
+  {
+    if(Wymiana[1] == 0)
+      return false; //duże zwierzę jest królikiem
+    if(Stado[0][Wymiana[1]] == 0)
+      return false; //stado główne nie posiada żądanego zwierzęcia
+    for(int i = 2; i <= 8; i++)
+      if(Wymiana[i] > Stado[id][i - 2])
+        return false; //gracz nie posiada oferowanej liczby zwierząt
+    int otrzymywane = 0;
+    for(int i = 2; i <= 8; i++)
+      otrzymywane += Wymiana[i]*krolikojednostki[i - 2];
+    if(otrzymywane < krolikojednostki[Wymiana[1]])
+      return false; //gracz żąda zwierzęcia oferując zwierzęta o zbyt małej wartości (względem królików wg tabeli przeliczników)
+    return true;
+  }
+}
+
+static bool stadoPoRzucie(int *Stado[5], int id, int kostka1, int kostka2)
+{
+  //kostka1: 1 - lis, 2/4/6/8/10/12 - królik, 3 - owca, 5 - świnia, 7 - koń, 9 - owca, 11 - świnia
+  //kostka2: 1/3/5/7/9/11 - królik, 2 - krowa, 4 - owca, 6 - świnia, 8 - owca, 10 - wilk, 12 - owca
+  int tab[5] = {0, 0, 0, 0, 0}; //kroliki, owce, swinie, krowy, konie (z kostek)
+  if(kostka1 % 2 == 0)
+    kostka1 = 2;
+  if(kostka2 % 2 == 1)
+    kostka2 = 1;
+  switch (kostka1)
+  {
+    case 1:
+      if(Stado[id][5] > 0){
+        Stado[id][5]--;
+        Stado[0][5]++;
+      }
+      else{
+        Stado[0][0] += Stado[id][0];
+        Stado[id][0] = 0;
+      }
+      break;
+    case 2:
+      tab[0]++;
+      break;
+    case 3:
+      tab[1]++;
+      break;
+    case 5:
+      tab[2]++;
+      break;
+    case 7:
+      tab[4]++;
+      break;
+    case 9:
+      tab[1]++;
+      break;
+    case 11:
+      tab[2]++;
+      break;
+    default:
+      break;
+  }
+  switch(kostka2)
+  {
+    case 1:
+      tab[0]++;
+      break;
+    case 2:
+      tab[3]++;
+      break;
+    case 4:
+      tab[1]++;
+      break;
+    case 6:
+      tab[2]++;
+      break;
+    case 8:
+      tab[1]++;
+      break;
+    case 10:
+      if(Stado[id][6] > 0){
+        Stado[id][6]--;
+        Stado[0][6]++;
+      }
+      else
+        for(int i = 1; i <= 3; i++){
+          Stado[0][i] += Stado[id][i];
+          Stado[id][i] = 0;
+        }
+      break;
+    case 12:
+      tab[1]++;
+      break;
+    default:
+      break;
+  }
+  bool czy_zmiana = false; //czy następuje zmiana w stadzie
+  //transfer zwierząt z głównego stada w wyniku rozmnażania (z zachowaniem możliwości zasobnych głównego stada)
+  for(int i = 0; i < 5; i++){
+    if(tab[i] > 0){
+      int x = Stado[id][i], y = Stado[0][i];
+      if(min((x + tab[i])/2, y) != 0)
+        czy_zmiana = true;
+      Stado[id][i] += min((x + tab[i])/2, y);
+      Stado[0][i] -= min((x + tab[i])/2, y);
+    }
+  }
+  return czy_zmiana;
+}
+
+static bool czyKoniecGry(int *Stado[5], int gracz)
+{
+  for(int j = 0; j <= 4; j++)
+    if(Stado[gracz][j] == 0)
+      return false;
+  return true;
+}
+
+static int* usadzGraczy(bool CzyCzlowiekGra)
+{
+  int *Usadzenie = new int[4]; //tabela zawierająca kolejność rozgrywki poczynając od gracza pod indeksem 1
+  //Gracze są oznaczani jako wartości: 1 - człowiek, 2 - bot królikowy, 3 - bot randomizowany, 4 - bot Usain Bolt, 5 - bot świński
+  int wykluczony = 1; //id gracza który nie weźmie udziału w danej rozgrywce, defaultowo człowiek
+  if(CzyCzlowiekGra)
+    wykluczony = 2 + losowanko()%4;
+  int cnt = 0;
+  for(int i = 1; i <= 5; i++)
+    if(i != wykluczony)
+      Usadzenie[cnt++] = i;
+  random_shuffle(Usadzenie, Usadzenie + 4);
+  for(int i = 0; i < 4; i++)
+    switch (Usadzenie[i])
+    {
+      case 1:
+        bots[i] = dokonajWymiany;
+        break;
+      case 2:
+        bots[i] = krolikowy;
+        break;
+      case 3:
+        bots[i] = randomizowany;
+        break;
+      case 4:
+        bots[i] = botQn;
+        break;
+      case 5:
+        bots[i] = swinski;
+        break;
+      default:
+        break;
+    }
+  return Usadzenie;
+}
+
+static int** inicjujStada()
+{
+  int **Stado = new int*[5];
+  for(int i = 0; i <= 4; i++)
+    Stado[i] = new int[7];//0 - króliki, 1 - owce, 2 - świnie, 3 - krowy, 4 - konie, 5 - małe psy, 6 - duże psy
+  Stado[0][0] = 60; Stado[0][1] = 24; Stado[0][2] = 20; Stado[0][3] = 12; Stado[0][4] = 6; Stado[0][5] = 4; Stado[0][6] = 2;
+  for(int i = 1; i <= 4; i++){
+    Stado[i][0] = 1;
+    for(int j = 1; j <= 6; j++)
+      Stado[i][j] = 0;
+  }
+  return Stado;
+}
+
+void rozpocznijGre(bool czyCzlowiekGra, string nazwaPlikuDoZapisu)
+{
+  zapisDoPliku.open(nazwaPlikuDoZapisu);
+  slosuj();
+  Usadzenie = usadzGraczy(czyCzlowiekGra);
+  Stado = inicjujStada();
+  Wymiana = new int[9];
+  inicjujZapisDoPliku(zapisDoPliku, Usadzenie);
+  for(int i = 0; i < 4; i++)
+    if(Usadzenie[i] == 1)
+      czlowiek = i + 1;
+  //cout << "Jesteś graczem numer: " << czlowiek << '\n';
+
+}
+
+/*int main(int argc, char *argv[]) //w wersji release nie będzie tej funkcji - służy testowaniu hosta
+{
+  //srand(getpid() + time(NULL));
+  string s;
+  if(argc < 2)
+    s = "dane1.csv";
+  else{
+    s = argv[1];
+    s="dane"+s+".csv";
+  }
+  rozpocznijGre(true, s);
+}*/
+/*
+ while(zwyciezca == -1){
+    bool czyzmiana = false;
+    nrrundy++;
+    for(int i = 0; i < 4; i++){
+      kodWymiany = bots[i](Stado, i + 1);
+      if(kodWymiany & (1LL << 32)){ //gracz żąda wymiany
+        konwersjaKoduWymiany(Wymiana, kodWymiany);
+        if(!czyWymianaPoprawna(Stado, i + 1, Wymiana)){
+          if(Usadzenie[i] != 1){
+            //Koniec_Gry(-1, false); //zakończenie rozgrywki z powodu błędu bota
+            //return;
+          }
+          else{ //wyświetlenie błędu żądania wymiany złożonej przez gracza i ponowienie możliwości wymiany
+            i--;
+            wymianaOdrzucona();
+            continue;
+          }
+        }
+        else //wymiana poprawna, dokonanie zmian w stadach
+        {
+          czyzmiana = true;
+          int kupno = (Wymiana[0] == 1)?1:-1;
+          Stado[i + 1][Wymiana[1]] += kupno;
+          Stado[0][Wymiana[1]] -= kupno;
+          for(int j = 2; j <= 8; j++){
+            Stado[i + 1][j - 2] -= kupno*Wymiana[j];
+            Stado[0][j - 2] += kupno*Wymiana[j];
+          }
+          WypiszPlansze(Stado, czlowiek);
+        }
+      }
+      //rzut kostką, zmiana w stadzie wynikająca z rzutu
+      kostka1 = 1 + losowanko()%12;
+      kostka2 = 1 + losowanko()%12;
+      czyzmiana |= stadoPoRzucie(Stado, i + 1, kostka1, kostka2);
+      wyswietlWynikRzutu(Stado, i + 1, kostka1, kostka2);
+      if(czyKoniecGry(Stado, i + 1)){
+        zwyciezca = i + 1;
+        break;
+      }
+    }
+    zapiszRunde(zapisDoPliku, Stado, nrrundy);
+    if(!czyzmiana)
+      cnt++;
+    else
+      cnt = 0;
+    if(cnt > 6){ //liczba rund bez zmiany stanu --> remis
+      zwyciezca = 0; //remis
+      break;
+    }
+  }
+  Koniec_Gry(zwyciezca, ((Usadzenie[zwyciezca - 1] == 1)?true:false));
+  for(int i = 0; i < 5; i++) //czyszczenie pamięci ze sterty
+    delete[] Stado[i];
+  delete[] Stado;
+  delete[] Usadzenie;
+  delete[] Wymiana;
+  zapisDoPliku.close();
+*/
