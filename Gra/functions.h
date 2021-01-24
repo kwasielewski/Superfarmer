4cf58705e4bb38998a3f72eb826b109c6e105140
Krzysiek <krzychuwas@gmail.com>
2021-01-24 18:24:58 +0100

    Usunięcie zbędnych kopii

--- Gra/functions.h
+++
@@ -1,3 +0,0 @@
-long long int decisionMaker(int exchange, int buy, int big, int rabbit, int sheep, int pig, int cow, int horse, int sDog, int bDog, int* s[5]);
-
-long long int possible(long long int decision, int* s[5]);
